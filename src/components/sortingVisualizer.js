5b4e85bab38dbf11c8ad5ae9059d93328ff5118d
LouisVelz <velazquezlouis2@gmail.com>
2021-04-02 00:00:34 -0400

    start columns

--- src/components/sortingVisualizer.js
+++ src/components/sortingVisualizer.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Styled from "styled-components";
-
-const Container = Styled.div``;
-const Element = Styled.div``;
+import { fisherYates } from "./../util/fisherYates";
+const Container = Styled.div`
+display: flex;
+flex-direction: row;
+border: 1px solid black;
+justify-content: center;
+`;
+const Element = Styled.div`
+width: 20px;
+height: ${(props) => props.height * 20};
+border: 1px solid gray;
+margin: 2px;
+`;
 
 const Visualizer = () => {
-  return "Hi this is visualizer";
+  const arr = fisherYates(10);
+
+  return (
+    <Container>
+      {arr.map((element) => {
+        return <Element height={element}></Element>;
+      })}
+    </Container>
+  );
 };
 
 export default Visualizer;
